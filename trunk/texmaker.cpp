svn2github/texstudio

commit 27e5e64a4f28887036d22d815dfead0b94ccfa21

implemented magic comments (Highlighting, display in structure). Currently supported: % !TeX spellcheck = someLanguage % !TeX encoding = someEncoding
Reworked dictionary support:
- dictionary can now be set per document
- manager to maintain multiple open dictionaries for different documents

git-svn-id: http://svn.code.sf.net/p/texstudio/code@2317 92cce386-e96d-4106-809f-596c673a3fd8

trunk/texmaker.cpp
@@ -61,7 +61,6 @@ Texmaker::Texmaker(QWidget *parent, Qt::WFlags flags)
 	svndlg=0;
 	userMacroDialog = 0;
 	mCompleterNeedsUpdate=false;
-	comboSpellHeight=0;
 	latexStyleParser=0;
 
 	ReadSettings();
@@ -83,13 +82,8 @@ Texmaker::Texmaker(QWidget *parent, Qt::WFlags flags)
 
 	importCwlAliases();
 
-	mainSpeller=new SpellerUtility();;
-	mainSpeller->loadDictionary(configManager.spell_dic,configManager.configFileNameBase);
-
-	LatexEditorView::setSpeller(mainSpeller);
-
 	QDocument::setFormatFactory(m_formats);
-	mainSpeller->spellcheckErrorFormat=m_formats->id("spellingMistake");
+	SpellerUtility::spellcheckErrorFormat = m_formats->id("spellingMistake");
 
 	if (configManager.autodetectLoadedFile) QDocument::setDefaultCodec(0);
 	else QDocument::setDefaultCodec(configManager.newFileEncoding);
@@ -814,15 +808,6 @@ void Texmaker::setupToolBars() {
 				QToolButton* combo=createComboToolButton(mtb.toolbar,list,0,this,SLOT(insertXmlTagFromToolButtonAction()));
 				combo->setProperty("tagsID", actionName);
 				mtb.toolbar->addWidget(combo);
-			} else if (actionName == "list/dictionaries"){
-				QStringList list;
-				QDir fic=QFileInfo(configManager.spell_dic).absoluteDir();
-				if (fic.exists() && fic.isReadable())
-					list << fic.entryList(QStringList("*.dic"),QDir::Files,QDir::Name);
-
-				comboSpell=createComboToolButton(mtb.toolbar,list,0, this,SLOT(SpellingLanguageChanged()),QFileInfo(configManager.spell_dic).fileName());
-				mtb.toolbar->addWidget(comboSpell);
-				addAction(mtb.toolbar->toggleViewAction());
 			} else {
 				QObject *obj=configManager.menuParent->findChild<QObject*>(actionName);
 				QAction *act=qobject_cast<QAction*>(obj);
@@ -854,9 +839,21 @@ void Texmaker::setupToolBars() {
 	}
 }
 
+void Texmaker::UpdateAvailableLanguages() {
+	foreach (QAction *act, statusTbLanguage->actions()) {
+		statusTbLanguage->removeAction(act);
+		delete act;
+	}
+	foreach (QString s, spellerManager.availableDicts()) {
+		QAction *act = new QAction(s, statusTbLanguage);
+		connect(act, SIGNAL(triggered()), this, SLOT(ChangeEditorSpeller()));
+		statusTbLanguage->addAction(act);
+	}
+}
 
 void Texmaker::createStatusBar() {
 	QStatusBar * status=statusBar();
+	status->setContextMenuPolicy(Qt::PreventContextMenu);
 
 	QAction *act;
 	QToolButton *tb;
@@ -865,29 +862,40 @@ void Texmaker::createStatusBar() {
 		tb = new QToolButton(status);
 		tb->setCheckable(true);
 		tb->setChecked(act->isChecked());
+		tb->setAutoRaise(true);
 		tb->setIcon(act->icon());
 		tb->setToolTip(act->toolTip());
 		connect(tb, SIGNAL(clicked()), act, SLOT(trigger()));
 		connect(act, SIGNAL(toggled(bool)), tb, SLOT(setChecked(bool)));
 		status->addPermanentWidget(tb, 0);
 	}
-
 	act = getManagedAction("main/view/outputview");
 	if (act) {
 		tb = new QToolButton(status);
 		tb->setCheckable(true);
 		tb->setChecked(act->isChecked());
+		tb->setAutoRaise(true);
 		tb->setIcon(act->icon());
 		tb->setToolTip(act->toolTip());
 		connect(tb, SIGNAL(clicked()), act, SLOT(trigger()));
 		connect(act, SIGNAL(toggled(bool)), tb, SLOT(setChecked(bool)));
 		status->addPermanentWidget(tb, 0);
-
 	}
 
-	// spacer eating up all the space between "left" and "right" permanent widgets. Could be used for status text in the future
-	QLabel* dummy = new QLabel(status);
-	status->addPermanentWidget(dummy, 1);
+	// spacer eating up all the space between "left" and "right" permanent widgets.
+	QLabel* messageArea = new QLabel(status);
+	connect(status, SIGNAL(messageChanged(QString)), messageArea, SLOT(setText(QString)));
+	status->addPermanentWidget(messageArea, 1);
+
+	statusTbLanguage = new QToolButton(status);
+	statusTbLanguage->setToolTip(tr("Language"));
+	statusTbLanguage->setPopupMode(QToolButton::InstantPopup);
+	statusTbLanguage->setAutoRaise(true);
+	statusTbLanguage->setMinimumWidth(status->fontMetrics().width("OOOOOO"));
+	connect(&spellerManager, SIGNAL(dictPathChanged()), this, SLOT(UpdateAvailableLanguages()));
+	UpdateAvailableLanguages();
+	statusTbLanguage->setText(spellerManager.defaultSpellerName());
+	status->addPermanentWidget(statusTbLanguage,0);
 
 	statusLabelMode=new QLabel(status);
 	statusLabelProcess=new QLabel(status);
@@ -895,7 +903,6 @@ void Texmaker::createStatusBar() {
 	status->addPermanentWidget(statusLabelEncoding,0);
 	status->addPermanentWidget(statusLabelProcess,0);
 	status->addPermanentWidget(statusLabelMode,0);
-	//statCursor=new QLabel( status );
 	for (int i=1; i<=3; i++) {
 		QPushButton* pb=new QPushButton(QIcon(QString(":/images/bookmark%1.png").arg(i)),"",status);
 		pb->setToolTip(tr("Click to jump to the bookmark"));
@@ -953,6 +960,10 @@ void Texmaker::editorTabChanged(int index){
 	if (index < 0) return; //happens if no tab is open
 	if (configManager.watchedMenus.contains("main/view/documents"))
 		updateToolBarMenu("main/view/documents");
+	if (currentEditorView()) {
+		QString spellerName = currentEditorView()->getSpeller()->name();
+		statusTbLanguage->setText(spellerName);
+	}
 }
 
 void Texmaker::EditorTabMoved(int from,int to){
@@ -1071,6 +1082,11 @@ void Texmaker::configureNewEditorView(LatexEditorView *edit) {
 	connect(edit->editor,SIGNAL(fileReloaded()),this,SLOT(fileReloaded()));
 	connect(edit->editor,SIGNAL(fileInConflict()),this,SLOT(fileInConflict()));
 	connect(edit->editor,SIGNAL(fileAutoReloading(QString)),this,SLOT(fileAutoReloading(QString)));
+
+	connect(edit, SIGNAL(spellerChanged(QString)), this, SLOT(EditorSpellerChanged(QString)));
+	edit->setSpellerManager(&spellerManager);
+	edit->setSpeller(spellerManager.defaultSpellerName());
+
 
 	edit->setBibTeXIds(&documents.allBibTeXIds);
 }
@@ -1779,10 +1795,7 @@ bool Texmaker::canCloseNow(){
 	bool accept = closeAllFilesAsking();
 	if (accept){
 		if (userMacroDialog) delete userMacroDialog;
-		if (mainSpeller) {
-			delete mainSpeller; //this saves the ignore list
-			mainSpeller=0;
-		}
+		spellerManager.unloadAll();  //this saves the ignore list
 	}
 	return accept;
 }
@@ -2034,7 +2047,7 @@ void Texmaker::editSpell() {
 		txsWarning(tr("No document open"));
 		return;
 	}
-	if (!spellDlg) spellDlg=new SpellerDialog(this,mainSpeller);
+	if (!spellDlg) spellDlg=new SpellerDialog(this, currentEditorView()->getSpeller());
 	spellDlg->setEditorView(currentEditorView());
 	spellDlg->startSpelling();
 }
@@ -2097,7 +2110,7 @@ void Texmaker::editInsertUnicode(){
 	connect(uid, SIGNAL(destroyed()), currentEditor(), SLOT(setFocus()));
 	connect(currentEditor(), SIGNAL(cursorPositionChanged()), uid, SLOT(close()));
 	connect(currentEditor(), SIGNAL(visibleLinesChanged()), uid, SLOT(close()));
-	connect(currentEditor()->document(), SIGNAL(contentsChanged()), uid, SLOT(close()));
+	connect(currentEditor()->document(), SIGNAL(()), uid, SLOT(close()));
 
 	uid->move(currentEditor()->mapTo(uid->parentWidget(), offset));
 	this->unicodeInsertionDialog = uid;
@@ -2305,6 +2318,10 @@ void Texmaker::ReadSettings() {
 			i++;
 		}
 	}
+
+	spellerManager.setIgnoreFilePrefix(configManager.configFileNameBase);
+	spellerManager.setDictPath(configManager.spellDictDir);
+	spellerManager.setDefaultSpeller(configManager.spellLanguage);
 
 	ThesaurusDialog::prepareDatabase(configManager.thesaurus_database);
 
@@ -2519,6 +2536,7 @@ void Texmaker::clickedOnStructureEntry(const QModelIndex & index){
 		break;
 
 	case StructureEntry::SE_SECTION:
+	case StructureEntry::SE_MAGICCOMMENT:
 	case StructureEntry::SE_TODO:
 	case StructureEntry::SE_LABEL:{
 			int lineNr=-1;
@@ -3178,16 +3196,17 @@ void Texmaker::InsertPageRef() {
 	outputView->setMessage("\\pageref{key}");
 }
 
-void Texmaker::SpellingLanguageChanged() {
+void Texmaker::EditorSpellerChanged(const QString &name) {
+	statusTbLanguage->setText(name);
+}
+
+void Texmaker::ChangeEditorSpeller() {
 	QAction *action = qobject_cast<QAction *>(sender());
 	if (!action) return;
 	if (!currentEditorView()) return;
-	QString text=action->text();
-	comboSpell->setDefaultAction(action);
+	QString name=action->text();
 
-	QString baseName=QFileInfo(configManager.spell_dic).absolutePath();
-	configManager.spell_dic=baseName+"/"+text;
-	mainSpeller->loadDictionary(configManager.spell_dic,configManager.configFileNameBase);
+	currentEditorView()->setSpeller(name);
 }
 
 ///////////////TOOLS////////////////////
@@ -3796,8 +3815,10 @@ void Texmaker::GeneralOptions() {
 	bool inlineSyntaxChecking=configManager.editorConfig->inlineSyntaxChecking;
 	QStringList loadFiles=configManager.completerConfig->getLoadedFiles();
 	if (configManager.execConfigDialog()) {
-		mainSpeller->loadDictionary(configManager.spell_dic,configManager.configFileNameBase);
-		// refresh quick language selection combobox
+		spellerManager.setDictPath(configManager.spellDictDir);
+		spellerManager.setDefaultSpeller(configManager.spellLanguage);
+
+
 		if (configManager.autodetectLoadedFile) QDocument::setDefaultCodec(0);
 		else QDocument::setDefaultCodec(configManager.newFileEncoding);
 

trunk/additionaltranslations.cpp
@@ -1,152 +1,304 @@
 #undef UNDEFINED
-#ifdef UNDEFINED
-static const char* translations[] = {
-QT_TRANSLATE_NOOP("ConfigManager", "&Latex"), 
-QT_TRANSLATE_NOOP("ConfigManager", "AMS packages"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{document}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&Sectioning"), 
-QT_TRANSLATE_NOOP("ConfigManager", "part"), 
-QT_TRANSLATE_NOOP("ConfigManager", "chapter"), 
-QT_TRANSLATE_NOOP("ConfigManager", "section"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subsection"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subsubsection"), 
-QT_TRANSLATE_NOOP("ConfigManager", "paragraph"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subparagraph"), 
-QT_TRANSLATE_NOOP("ConfigManager", "part*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "chapter*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "section*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subsection*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subsubsection*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "paragraph*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "subparagraph*"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&Environments"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{<environment>} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{center} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{flushleft} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{flushright}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{quote}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{quotation}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{verse}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{verbatim}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{table}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{figure}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{titlepage}  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&List Environments"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{itemize}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{enumerate}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{description}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{list}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Font St&yles"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\emph - Emphasis  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textit - Italics  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textsl - Slanted  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textbf - Boldface  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\texttt - Typewriter  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textsc - Small caps  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\textsf - Sans Serif  [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\underline - Underline [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Font Sizes"), 
-QT_TRANSLATE_NOOP("ConfigManager", "tiny"), 
-QT_TRANSLATE_NOOP("ConfigManager", "scriptsize"), 
-QT_TRANSLATE_NOOP("ConfigManager", "footnotesize"), 
-QT_TRANSLATE_NOOP("ConfigManager", "small"), 
-QT_TRANSLATE_NOOP("ConfigManager", "normalsize"), 
-QT_TRANSLATE_NOOP("ConfigManager", "large"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Large"), 
-QT_TRANSLATE_NOOP("ConfigManager", "LARGE"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Huge"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&Tabular Environment"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{tabbing}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{tabular}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "S&pacing"), 
-QT_TRANSLATE_NOOP("ConfigManager", "New line"), 
-QT_TRANSLATE_NOOP("ConfigManager", "International &Accents"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\includegraphics{file}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\include{file}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\input{file}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Cross References"), 
-QT_TRANSLATE_NOOP("ConfigManager", "label"), 
-QT_TRANSLATE_NOOP("ConfigManager", "ref"), 
-QT_TRANSLATE_NOOP("ConfigManager", "pageref"), 
-QT_TRANSLATE_NOOP("ConfigManager", "index"), 
-QT_TRANSLATE_NOOP("ConfigManager", "cite"), 
-QT_TRANSLATE_NOOP("ConfigManager", "footnote"), 
-QT_TRANSLATE_NOOP("ConfigManager", "&Math"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Inline math mode $...$"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Display math mode \\[...\\]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Numbered equations \\begin{equation}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{align}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{eqnarray}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "_{} - subscript"), 
-QT_TRANSLATE_NOOP("ConfigManager", "^{} - superscript"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\begin{array}"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Math &Functions"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Math Font St&yles"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathrm{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathit{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathbf{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathsf{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathtt{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathcal{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathbb{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "\\mathfrak{} [selection]"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Math &Accents"), 
-QT_TRANSLATE_NOOP("ConfigManager", "Math S&paces"), 
-QT_TRANSLATE_NOOP("ConfigManager", "small"), 
-QT_TRANSLATE_NOOP("ConfigManager", "medium"), 
-QT_TRANSLATE_NOOP("ConfigManager", "large"), 
-QT_TRANSLATE_NOOP("ConfigManager", "quad"), 
-QT_TRANSLATE_NOOP("ConfigManager", "qquad"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "background"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:error"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:warning"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:badbox"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark0"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark1"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark2"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark3"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark4"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark5"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark6"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark7"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark8"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark9"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "commentTodo"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "comment"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "keyword"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "extra-keyword"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "math-keyword"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "verbatim"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "braceMatch"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "braceMismatch"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "search"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "numbers"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "text"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "escapeseq"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "styleHint"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "spellingMistake"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "latexSyntaxMistake"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "temporaryCodeCompletion"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "environment"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "referencePresent"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "referenceMissing"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "referenceMultiple"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "citationPresent"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "citationMissing"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "structure"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "current"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "selection"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "replacement"), 
-QT_TRANSLATE_NOOP("QFormatConfig", "normal"),
-QT_TRANSLATE_NOOP("Texmaker","Custom"),
-QT_TRANSLATE_NOOP("Texmaker","File"),
-QT_TRANSLATE_NOOP("Texmaker","Edit"),
-QT_TRANSLATE_NOOP("Texmaker","Tools"),
-QT_TRANSLATE_NOOP("Texmaker","Math"),
-QT_TRANSLATE_NOOP("Texmaker","Format"),
-QT_TRANSLATE_NOOP("Texmaker","Table"),
-QT_TRANSLATE_NOOP("Texmaker","Central"),
-""};
-#endif
+
+#ifdef UNDEFINED
+
+static const char* translations[] = {
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Latex"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "AMS packages"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{document}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Sectioning"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "part"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "chapter"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "section"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subsection"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subsubsection"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "paragraph"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subparagraph"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "part*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "chapter*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "section*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subsection*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subsubsection*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "paragraph*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "subparagraph*"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Environments"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{<environment>} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{center} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{flushleft} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{flushright}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{quote}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{quotation}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{verse}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{verbatim}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{table}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{figure}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{titlepage}  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&List Environments"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{itemize}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{enumerate}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{description}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{list}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Font St&yles"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\emph - Emphasis  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textit - Italics  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textsl - Slanted  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textbf - Boldface  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\texttt - Typewriter  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textsc - Small caps  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\textsf - Sans Serif  [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\underline - Underline [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Font Sizes"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "tiny"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "scriptsize"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "footnotesize"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "small"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "normalsize"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "large"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Large"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "LARGE"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Huge"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Tabular Environment"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{tabbing}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{tabular}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "S&pacing"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "New line"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "International &Accents"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\includegraphics{file}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\include{file}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\input{file}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Cross References"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "label"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "ref"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "pageref"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "index"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "cite"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "footnote"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "&Math"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Inline math mode $...$"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Display math mode \\[...\\]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Numbered equations \\begin{equation}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{align}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{eqnarray}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "_{} - subscript"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "^{} - superscript"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\begin{array}"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Math &Functions"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Math Font St&yles"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathrm{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathit{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathbf{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathsf{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathtt{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathcal{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathbb{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "\\mathfrak{} [selection]"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Math &Accents"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "Math S&paces"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "small"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "medium"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "large"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "quad"), 
+
+QT_TRANSLATE_NOOP("ConfigManager", "qquad"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "background"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:error"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:warning"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:badbox"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark0"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark1"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark2"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark3"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark4"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark5"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark6"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark7"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark8"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "line:bookmark9"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "commentTodo"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "magicComment"),
+
+QT_TRANSLATE_NOOP("QFormatConfig", "comment"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "keyword"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "extra-keyword"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "math-keyword"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "verbatim"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "braceMatch"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "braceMismatch"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "search"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "numbers"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "text"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "escapeseq"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "styleHint"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "spellingMistake"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "latexSyntaxMistake"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "temporaryCodeCompletion"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "environment"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "referencePresent"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "referenceMissing"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "referenceMultiple"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "citationPresent"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "citationMissing"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "structure"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "current"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "selection"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "replacement"), 
+
+QT_TRANSLATE_NOOP("QFormatConfig", "normal"),
+
+QT_TRANSLATE_NOOP("Texmaker","Custom"),
+
+QT_TRANSLATE_NOOP("Texmaker","File"),
+
+QT_TRANSLATE_NOOP("Texmaker","Edit"),
+
+QT_TRANSLATE_NOOP("Texmaker","Tools"),
+
+QT_TRANSLATE_NOOP("Texmaker","Math"),
+
+QT_TRANSLATE_NOOP("Texmaker","Format"),
+
+QT_TRANSLATE_NOOP("Texmaker","Table"),
+
+QT_TRANSLATE_NOOP("Texmaker","Central"),
+
+""};
+
+#endif
 

commit 89bbf760bcdb66b451d43b6c2f8009cb846d93e4

revert a unicode dialog signal broken in r2317
git-svn-id: http://svn.code.sf.net/p/texstudio/code@2887 92cce386-e96d-4106-809f-596c673a3fd8

trunk/texmaker.cpp
@@ -2392,7 +2392,7 @@ void Texmaker::editInsertUnicode(){
 	connect(uid, SIGNAL(destroyed()), currentEditor(), SLOT(setFocus()));
 	connect(currentEditor(), SIGNAL(cursorPositionChanged()), uid, SLOT(close()));
 	connect(currentEditor(), SIGNAL(visibleLinesChanged()), uid, SLOT(close()));
-	connect(currentEditor()->document(), SIGNAL(()), uid, SLOT(close()));
+	connect(currentEditor()->document(), SIGNAL(contentsChanged()), uid, SLOT(close()));
 	
 	uid->move(currentEditor()->mapTo(uid->parentWidget(), offset));
 	this->unicodeInsertionDialog = uid;